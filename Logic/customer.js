4074ebb7e3ccf25b84d5000514e83162a28990c4
Frederik Stapelfeld <fst@nextway.software>
2020-11-03 15:58:38 +0100

    day 2 of programming - no more xml, try fetch json

--- Logic/customer.js
+++ Logic/customer.js
@@ -1,19 +1,19 @@
 class Customer {
-    constructor(username, password, url) {
-      this._username = username;
-      this._password = password;
-      this._url = url;
-    }
-    present() {
-      return this._username + ':' + this._password + ' -> ' + this._url;
-    }
-    get username() {
-        return this._username;
-    }
-    get password() {
-        return this._password;
-    }
-    get url() {
-        return this._url;
-    }
+  constructor(username, password, url) {
+    this._username = username;
+    this._password = password;
+    this._url = url;
   }
+  present() {
+    return this._username + ":" + this._password + " -> " + this._url;
+  }
+  get username() {
+    return this._username;
+  }
+  get password() {
+    return this._password;
+  }
+  get url() {
+    return this._url;
+  }
+}
