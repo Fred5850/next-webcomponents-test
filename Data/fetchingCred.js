dc4fce94c0bad737e15294c7cb5d49f979de891e
Frederik Stapelfeld <fst@nextway.software>
2020-11-09 11:34:06 +0100

    service method added getItem

--- Data/fetchingCred.js
+++ Data/fetchingCred.js
@@ -13,20 +13,41 @@ function FetchingCredentials() {
       console.log(data);
       credentials = data.ticket;
       console.log(credentials);
+      //GetMetaData(credentials);
       console.log("FetchingCredentials called: End");
     });
 }
-//dagens credentials 05-11
-//9S2NGN2ZENS6WEKDENP78TB1E9HPGTBPCMX7AWV5E8X42H2D9570_GDVWF5B82EKJGAHSWV5AQG0
-function TakeCrednetialsAndGo(credentials) {
-  console.log("TakeCrednetialsAndGo called: Start");
+
+function getItem(credentials) {
   var url =
     "http://localhost:8080/rest/list/1/items/YEL/0/10/InvoiceId/385987?cred=" +
     credentials;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      console.log("TakeCrednetialsAndGo called: End");
-    });
+  return fetch(url).then((response) => response.json());
 }
+function getContent(credentials) {
+  getItem(credentials).then((data) => {
+    console.log(data);
+  });
+}
+//dagens credentials 05-11
+//9S2NGN2ZENS6WEKDENP78TB1E9HPGTBPCMX7AWV5E8X42H2D9570_GDVWF5B82EKJGAHSWV5AQG0
+function GetMetaData(credentials) {
+  console.log("GetMetaData called: Start");
+  getItem(credentials).then((data) => {
+    //console.log(data); // whole object
+    test = data.result[0].metadata;
+    test.forEach((obj) => {
+      //console.log(obj); //metadata
+      Object.entries(obj).forEach(([key, value]) => {
+        console.log(`${key} = ${value}`); //each key value pair in meta data
+      });
+      console.log("-------------------");
+    });
+    console.log(test);
+    console.log("GetMetaData called: End");
+  });
+}
+
+//     if ("label" == `${key}`) {
+//       console.log(`${key} = ${value}`);
+//     }
